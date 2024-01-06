024abdb8d87812af12a563f7efaf59ce514ef6d0
Yurivlj <yurivasconcelos756@gmail.comm>
2024-01-06 20:34:53 -0300

    Alteração3

--- Dessafio.py
+++ Dessafio.py
@@ -1,29 +1,29 @@
 nome = 'Yuri'
-xp_heroi = 100
+xp_heroi = 8000
 
 nivel = ""
 
-if xp_heroi < 1000:
+if xp_heroi <= 1000:
     nivel = 'Ferro'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 1.001 <= xp_heroi < 2.000:
+if 1001 <= xp_heroi <= 2000:
     nivel = 'Bronze'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 2.001 <= xp_heroi <= 5.000:
+if 2001 <= xp_heroi <= 6000:
     nivel = 'Prata'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 6.001 <= xp_heroi <= 7.000:
+if 6001 <= xp_heroi <= 7000:
     nivel = 'Ouro'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 7.001 <= xp_heroi <= 8.000:
+if 7001 <= xp_heroi <= 8000:
     nivel = 'Platina'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 8.001 <= xp_heroi <= 9.000:
+if 8001 <= xp_heroi <= 9000:
     nivel = 'Ascendente'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if 9.001 <= xp_heroi < 10.000:
+if 9001 <= xp_heroi <= 10000:
     nivel = 'Imortal'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
-if xp_heroi >= 10.001:
+if xp_heroi >= 10001:
     nivel = 'Radiante'
     print(f'Seu nome é {nome} e seu nível é {nivel}')
